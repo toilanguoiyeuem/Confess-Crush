bc32596de77b474ddea3ea2913994c0d9be15727
toilanguoiyeuem <101090883+toilanguoiyeuem@users.noreply.github.com>
2022-03-07 14:39:04 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Gui cho ban nao ten Quỳnh <3 ",
+    introTitle: 'Đào Hà Quỳnh',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
